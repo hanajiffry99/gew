991a5461ccbad7b735bdc74a17e4b96746c24c5b
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-31 01:07:53 +0530

    forum

---
+++ src/components/Posts/Time.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Time(props) {
+    // Function to Calculate Time
+    function timeSince(date) {
+        var seconds = Math.floor((new Date() - date) / 1000);
+        var interval = seconds / 31536000;
+        if (interval > 1) {
+          return Math.floor(interval) + " years";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+          return Math.floor(interval) + " months";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+          return Math.floor(interval) + " days";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+          return Math.floor(interval) + " hours";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+          return Math.floor(interval) + " minutes";
+        }
+        if(Math.floor(seconds)===0){
+          return "Just Now";
+        }
+        return Math.floor(seconds) + " seconds";
+      }
+
+
+    return (
+       <>
+        <div>{ props.postedByUser }
+          <span> { timeSince(new Date(props.timeStamp)) }</span>
+        </div>
+       </>
+    )
+}
+
+export default Time
