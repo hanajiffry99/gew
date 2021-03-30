4a6e476fbc112c5fe652e68dc451ec2e868a0889
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-31 01:13:47 +0530

    contact

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Forms from '../contact/Forms';
+
+function Contact() {
+    return (
+        <>
+        <div style={{textAlign:'center'}}>
+            Please feel free to give us your feedbacks and direct us your doubts
+        </div>
+        <Forms/>
+        </>
+    )
+}
+
+export default Contact
